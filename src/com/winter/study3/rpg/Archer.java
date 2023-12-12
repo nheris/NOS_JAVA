1098bf20b4906aa01e8f30ebb1868949efa379a8
Nohnon <arark057@gmail.com>
2023-12-12 19:14:13 +0900

    1

---
+++ src/com/winter/study3/rpg/Archer.java
@@ -0,0 +1,19 @@
+package com.winter.study3.rpg;
+
+public class Archer {
+	public Archer() {
+			
+			
+		}
+		
+		
+		//메서드 오버로딩 -> new 
+		//메서드 오버라이딩 -> 수정
+		//상속받은 메서드를 자기자신에 맞게 내용을 재정의 하는것
+		//메서드 선언부는 동일하게, 내용만 변경
+		public void attack() {
+			System.out.println("화살 쏘기");
+		}
+	
+}
+
